de7b359084965faaa6fe4066864694f82cee89f8
greenhornet63603 <greenhornet63603@gmail.com>
2025-11-26 18:19:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "20288951"
-api_hash = "e8cb5fb7a475b5f5eb3b0ef0e6ca03a8"
-bot_token = "8037871175:AAGnJzyLnvptj7c5pG-r3PrXeyZBIaeInVo"
-auth_users = [1671836568]
+api_id = "27400172"
+api_hash = "56d0a75c5f9a9de6beb5452aa63c2d36"
+bot_token = "8458189269:AAHtEEHZnhJPWg43SuGCDvMrfz18wMBN-MY"
+auth_users = [7540570087]
